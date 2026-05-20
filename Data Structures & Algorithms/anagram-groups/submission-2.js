d8a454886f90749e439f601ed7d2d1c86fe00ea9
Rhys Mahannah <97256979+rhysmah@users.noreply.github.com>
2026-05-20 10:00:13 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const results = new Map();
+    
+        for (const str of strs) {
+            let sorted = Array.from(str).sort().join("");
+            
+            if (!results.has(sorted)) {
+                results.set(sorted, [str]);
+            } else {
+                const resultsSoFar = results.get(sorted);
+                results.set(sorted, [...results.get(sorted), str])
+            }
+        }   
+        return Array.from(results.values());
+    }
+}
