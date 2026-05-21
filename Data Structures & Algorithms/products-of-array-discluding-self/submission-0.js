b5ce63cfec9d8a2b0cbaf3dd4911001c1e4a7742
Rhys Mahannah <97256979+rhysmah@users.noreply.github.com>
2026-05-21 11:11:39 -0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const result = [];
+
+        let left = 1;
+        for (let i = 0; i < nums.length; i++) {
+            result[i] = left;
+            left *= nums[i];
+        }
+
+        let right = 1;
+        for (let i = nums.length - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+
+        return result;
+
+    }
+}
