86dfa6da428115576b06ad390d48c5f4638be916
Rhys Mahannah <97256979+rhysmah@users.noreply.github.com>
2026-06-11 10:06:34 -0700

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false;
+        
+        const letters = new Map();
+
+        for (let i = 0; i < s.length; i++) {
+            letters.set(s[i], (letters.get(s[i]) || 0) + 1)
+        }
+
+        for (let i = 0; i < t.length; i++) {
+            if (!letters.has(t[i])) return false;
+            letters.set(t[i], (letters.get(t[i]) - 1));
+            
+            if (letters.get(t[i]) < 0) return false;
+        }
+
+        return true;
+    }
+}
